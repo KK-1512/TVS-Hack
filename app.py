b91d286d43ca20abdff05d571413728024d8d184
Krishnakumar V <krishna.v15122004@gmail.com>
2026-03-18 11:31:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -84,29 +84,7 @@ st.image(
     "TVS_Hack.png",
     use_container_width=True
 )
-st.markdown("")
-from datetime import datetime
-import pytz
-import time
 
-# Placeholder for live time
-time_placeholder = st.empty()
-
-# Indian Time Zone
-ist = pytz.timezone('Asia/Kolkata')
-
-# Live update loop
-while True:
-    now = datetime.now(ist)
-    
-    current_time = now.strftime("%d %B %Y | %I:%M:%S %p")
-    
-    time_placeholder.markdown(
-        f"<h4 style='text-align:center; color:#00bfff;'>{current_time}</h4>",
-        unsafe_allow_html=True
-    )
-    
-    time.sleep(1)
 # -----------------------------------
 # Input Section (SAME AS OLD, BUT TYPING INPUT)
 # -----------------------------------
@@ -210,3 +188,26 @@ st.markdown(
 st.caption(
     "College of Engineering, Guindy | Intelligent Vehicle Response-Based Road Profiling System"
 )
+st.markdown("")
+from datetime import datetime
+import pytz
+import time
+
+# Placeholder for live time
+time_placeholder = st.empty()
+
+# Indian Time Zone
+ist = pytz.timezone('Asia/Kolkata')
+
+# Live update loop
+while True:
+    now = datetime.now(ist)
+    
+    current_time = now.strftime("%d %B %Y | %I:%M:%S %p")
+    
+    time_placeholder.markdown(
+        f"<h4 style='text-align:center; color:#00bfff;'>{current_time}</h4>",
+        unsafe_allow_html=True
+    )
+    
+    time.sleep(1)
